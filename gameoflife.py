d88d9bc68859dca792e090eba7555a0a76907b71
Ralfs Sedlenieks <rufle23@gmail.com>
2022-04-18 19:07:22 +0300

    Added states and examples

--- gameoflife.py
+++ gameoflife.py
@@ -2,6 +2,7 @@ import argparse
 import numpy
 import os
 import sys
+import time
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "no"
 
@@ -25,6 +26,9 @@ def calculate_frame():
     cur_i = 0 - multiplier
     cur_j = 0 - multiplier
 
+    global random_matrix
+    new_matrix = numpy.copy(random_matrix)
+
     for i in range(height):
         cur_i = cur_i + multiplier
         for j in range(width):
@@ -33,12 +37,13 @@ def calculate_frame():
             square.fill(BLACK if cur_col == 0 else WHITE)
             cur_px = pygame.Rect(cur_i, cur_j, multiplier, multiplier)
             display_screen.blit(square, cur_px)
-            update_cell(i, j)
+            update_cell(i, j, new_matrix)
         cur_j = 0 - multiplier
     pygame.display.flip()
+    random_matrix = numpy.copy(new_matrix)
 
 
-def update_cell(x, y):
+def update_cell(x, y, new_matrix):
     cell_alive = True if random_matrix[x][y] == 1 else False
     n1 = get_neighbour(x-1, y-1)
     n2 = get_neighbour(x, y-1)
@@ -59,17 +64,18 @@ def update_cell(x, y):
         else:
             live_neighbours += 1
 
+    # NB: births and deaths occur simultaneously
     # Rules:
     # Any live cell with two or three live neighbours survives.
     # Any dead cell with three live neighbours becomes a live cell.
     # All other live cells die in the next generation. Similarly, all other dead cells stay dead.
 
     if cell_alive and (live_neighbours == 2 or live_neighbours == 3):
-        random_matrix[x][y] = 1
-    elif not cell_alive and live_neighbours == 3:
-        random_matrix[x][y] = 1
+        new_matrix[x][y] = 1
+    elif (not cell_alive) and live_neighbours == 3:
+        new_matrix[x][y] = 1
     else:
-        random_matrix[x][y] = 0
+        new_matrix[x][y] = 0
 
 
 def get_neighbour(x1, y1):
@@ -81,32 +87,65 @@ def get_neighbour(x1, y1):
         return random_matrix[x1][y1]
 
 
+def save_state():
+    out_name = "state-{}.txt".format(int(time.time()))
+    with open(out_name, 'w') as out_file:
+        out_file.write("{0} {1} {2}\n".format(height, width, multiplier))
+        for a in range(height):
+            for b in range(width):
+                if random_matrix[a][b] == 1:
+                    out_file.write("{0} {1}\n".format(a, b))
+
+
+def load_state(path):
+    with open(path, 'r') as in_file:
+        global height
+        global width
+        global multiplier
+        global window_width
+        global window_height
+        global random_matrix
+
+        params = in_file.readline().split(' ')
+        height = int(params[0])
+        width = int(params[1])
+        multiplier = int(params[2])
+        window_height = height*multiplier
+        window_width = width*multiplier
+
+        zero_matrix = numpy.random.randint(0, 1, (height, width))
+
+        for line in in_file:
+            coords = line.split(" ")
+            x = int(coords[0])
+            y = int(coords[1])
+            zero_matrix[x][y] = 1
+
+        random_matrix = zero_matrix
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('height', help='Specify the height', metavar='HEIGHT', default=100, nargs='?')
     parser.add_argument('width', help='Specify the width', metavar='WIDTH', default=100, nargs='?')
     parser.add_argument('multiplier', help='Specify the pixel multiplier', metavar='MULTIPLIER', default=1, nargs='?')
+    parser.add_argument('-f', '--file', help='Specify the input file', metavar='FILE', required=False)
     args = parser.parse_args()
 
-    # TODO: add seed and option for random start
+    filename = args.file
 
-    height = int(args.height)
-    width = int(args.width)
-    multiplier = int(args.multiplier)
-    window_height = height * multiplier
-    window_width = width * multiplier
-
-    random_matrix = numpy.random.randint(0, 2, (height, width))
-
-    # TODO: config param
-    mask_times = 3
-
-    for n in range(mask_times):
-        mask = numpy.random.randint(0, 2, (height, width))
-        for h in range(height):
-            for w in range(width):
-                if random_matrix[h][w] and mask[h][w]:
-                    random_matrix[h][w] = 0
+    if filename:
+        if not os.path.isabs(filename):
+            infile = os.path.join(sys.path[0], filename)
+        load_state(filename)
+    else:
+        height = int(args.height)
+        width = int(args.width)
+        multiplier = int(args.multiplier)
+        window_height = height * multiplier
+        window_width = width * multiplier
+        random_matrix = numpy.random.randint(0, 2, (height, width))
+        save_state()
 
     pygame.init()
 
